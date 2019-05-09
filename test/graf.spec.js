dc71991debe148f34273440e3e7fdd92f79e0867
Tyler Whited <whitedtd@gmail.com>
2019-05-08 21:28:40 -0600

    Add test to calculate route weight given a list of nodes isntead of edges

--- test/graf.spec.js
+++ test/graf.spec.js
@@ -4,6 +4,7 @@ const { expect } = require('chai');
 const {
   grafFactory,
   getRouteWeight,
+  getNodesRouteWeight,
 } = require('../graf/graf');
 
 const {
@@ -11,11 +12,21 @@ const {
 } = require('./graf-mocks');
 
 describe('Tests graph static (non-pathfinding) methods', () => {
-  let graf, edges, weight;
-  it('Tests edge weight summation', () => {
+  let graf, edges, nodes, weight;
+
+  beforeEach(() => {
     graf = grafFactory(grafEdgeMock1);
+  });
+
+  it('Tests edge weight summation', () => {
     edges = graf.edges;
     weight = getRouteWeight(edges);
+    expect(weight).to.equal(6);
+  });
+
+  it('Tests edge weight summation given list of nodes', () => {
+    nodes = graf.nodes;
+    weight = getNodesRouteWeight(nodes, graf);
     expect(weight).to.equal(6);
   });
 });
