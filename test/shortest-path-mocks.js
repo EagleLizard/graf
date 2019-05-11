69bcfe86616c22f62aef41f67b0d52e86a97ceee
Tyler Whited <whitedtd@gmail.com>
2019-05-10 21:15:15 -0600

    Prep for adding heavily connected grid graf

--- test/shortest-path-mocks.js
+++ test/shortest-path-mocks.js
@@ -84,7 +84,54 @@ const gridGrafMockB = {
   ],
 };
 
+const gridGrafMockC = {
+  nodes: [
+    [
+      ['a'],
+      ['b'],
+      ['c'],
+      ['d'],
+    ],
+    [
+      ['e'],
+      ['f'],
+      ['g'],
+      ['h'],
+    ],
+    [
+      ['i'],
+      ['j'],
+      ['k'],
+      ['l'],
+    ],
+    [
+      ['m'],
+      ['n'],
+      ['o'],
+      ['p'],
+    ],
+  ],
+  edges: [
+    [ 'a', 'b' ],
+    [ 'b', 'c' ],
+    [ 'c', 'd' ],
+    [ 'a', 'e' ],
+    [ 'c', 'g' ],
+    [ 'd', 'h' ],
+    [ 'e', 'f' ],
+    [ 'h', 'l' ],
+    [ 'i', 'j' ],
+    [ 'j', 'k' ],
+    [ 'k', 'l' ],
+    [ 'i', 'm' ],
+    [ 'm', 'n' ],
+    [ 'n', 'o' ],
+    [ 'o', 'p' ],
+  ],
+};
+
 module.exports = {
   gridGrafMockA,
   gridGrafMockB,
+  gridGrafMockC,
 };
